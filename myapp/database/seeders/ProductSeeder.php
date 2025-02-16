c5c6ac0de20e823f5e8ff67a6be06aeec521fe3b
Aymane <aymanee.najibi@gmail.com>
2025-02-16 18:36:45 +0100

    Générer des Produits  Avec Factory (Product::factory())

--- myapp/database/seeders/ProductSeeder.php
+++ myapp/database/seeders/ProductSeeder.php
@@ -10,15 +10,26 @@ class ProductSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Faker::create();
 
-        for ($i = 0; $i < 100; $i++) {
+        //* Vider la table avant d'insérer de nouvelles données
+        Product::truncate();
+
+        //* Utilise ProductFactory.php
+        Product::factory()->count(100)->create();
+
+        /*
+
+        * methode de creer un faker produits avec un boucle for
+        * sans utiliser ProductFactory.php
+         $faker = Faker::create();
+
+         for ($i = 0; $i < 100; $i++) {
             Product::create([
                 'title' => $faker->word,
                 'price' => $faker->randomFloat(2, 10, 500),
                 'description' => $faker->sentence,
                 'discount' => $faker->numberBetween(0, 50),
-            ]);
-        }
+                ]);
+            } */
     }
 }
