b2db6b900fd0cef9b8823e307e61cb8923d7c7cf
Fahd EL yakhlifi <yakhlifif@gmail.com>
2025-02-22 17:20:13 +0100

    ajouter bootstrap sur views (name,user)

--- myapp/resources/views/tp0/name.blade.php
+++ myapp/resources/views/tp0/name.blade.php
@@ -1,4 +1,4 @@
-@extends ('layout')
+@extends ('layouts.bootstrap')
 @section('content')
     <h1 class="text-center text-primary fw-bold">
         Bonjour {{ $genre . " " . $n }}
--- myapp/resources/views/tp1/user.blade.php
+++ myapp/resources/views/tp1/user.blade.php
@@ -1,4 +1,4 @@
-@extends ('layout')
+@extends ('layouts.bootstrap')
 @section('content')
 
 
